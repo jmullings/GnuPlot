8ef53df447d90b1c18f5155c30d37e1ad7074a2a
Jason Mulings <j_mullings@hotmail.co.uk>
2016-05-26 20:13:13 +1200

    Demo3D File
    Auto range added for simplicity

---
+++ demo/GNUPlot3D.Demo.php
@@ -0,0 +1,24 @@
+<?php
+
+include ".../Gregwar/GnuPlot/GnuPlot.php";
+
+
+use Gregwar\GnuPlot\GnuPlot;
+
+$alpha = 0;
+$plot = new GnuPlot;
+
+$vec1 = [100,100,100];
+$vec2 = [200,200,200];
+$vec3 = [-200,200,200];
+usleep(50000);
+$plot->reset();
+$plot->setGraphTitle('The XYZ 3D plot');
+$plot->setTitle(0, 'The visual vector');
+
+$plot->setArrowFrom($vec1);
+$plot->setArrowFrom($vec2);
+$plot->setArrowFrom($vec3);
+
+$plot->refresh();
+sleep(1000);
